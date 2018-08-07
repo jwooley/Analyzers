fc4fb912fdf37340fcb422b0e283999efd7238fe
Jim Wooley <jimwooley@hotmail.com>
2018-08-06 21:42:48 -0400

    Add sample webapi project for use debugging the mvc analyzer

---
+++ WebApplication1/WebApplication1/Controllers/ApiControllerMisnamed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+	public class ApiControllerMisnamed : ApiController
+	{
+		public ApiControllerMisnamed()
+		{
+
+		}
+	}
+}
