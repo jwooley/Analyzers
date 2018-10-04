8df55563d6af508ec17a8182b7bd2c43def5886c
Jim Wooley <jimwooley@hotmail.com>
2018-10-04 14:20:53 -0400

    Add IDisposable demo setup

--- CsDemos/CodeCrackerSamples.cs
+++ CsDemos/CodeCrackerSamples.cs
@@ -39,7 +39,7 @@ namespace CodeCrackerSamples
         { Console.WriteLine(theAnswer); }
 
     }
-    public class Person
+    public class Person : IDisposable
     {
         private string _name;
         public string Name
@@ -116,5 +116,40 @@ namespace CodeCrackerSamples
                 new Person()
             }).OrderBy(p => p.Name).OrderBy(p => p.Age);
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Person() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
